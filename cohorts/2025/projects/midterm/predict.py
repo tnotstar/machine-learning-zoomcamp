ec39dc742de0fec617b804013f9425d6b3715268
Antonio Alvarado <tnotstar@gmail.com>
2025-11-18 15:32:50 +0000

    Add additional field to transaction object

--- cohorts/2025/projects/midterm/predict.py
+++ cohorts/2025/projects/midterm/predict.py
@@ -8,9 +8,24 @@ from pydantic import BaseModel
 
 
 class TransactionRequest(BaseModel):
-    transaction_amount: float
-    account_balance: float
-    risk_score: float
+    transaction_amount: float = 200.0
+    account_balance: float = 0.0
+    daily_transaction_count: int = 0
+    avg_transaction_amount_7d: float = 15
+    failed_transaction_count_7d: int = 1
+    card_age: float = 2
+    transaction_distance: float = 1000
+    risk_score: float = 0.1
+    authentication_method: str = "Password"
+    card_type: str = "Visa"
+    device_type: str = "Mobile"
+    hour_of_day: int = 10
+    ip_address_flag: int = 0
+    is_weekend: int = 0
+    location: str = "New York"
+    merchant_category: str = "Electronics"
+    previous_fraudulent_activity: int = 0
+    transaction_type: str = "Online"
 
 
 class PredictionResponse(BaseModel):
